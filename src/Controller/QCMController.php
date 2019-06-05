bffffdd24b326b50c6424558be570e0bb965eca8
Elodie Billon <elodie.billon1995@outlook.fr>
2019-06-05 13:46:07 +0200

    Importation de la bdd, mise en forme commencée

--- src/Controller/QCMController.php
+++ src/Controller/QCMController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Repository\QCMAnswersRepository;
 use App\Repository\QCMQuestionsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class QCMController extends AbstractController
@@ -12,11 +13,11 @@ class QCMController extends AbstractController
     /**
      * @Route("/qcm", name="q_c_m")
      */
-    public function index(QCMAnswersRepository $answersRepository, QCMQuestionsRepository $questionsRepository)
+    public function index(QCMAnswersRepository $answersRepository, QCMQuestionsRepository $questionsRepository):Response
     {
         return $this->render('qcm/index.html.twig', [
-            'answer' => $answersRepository->findAll(),
-            'question' => $questionsRepository->findAll(),
+            'answers' => $answersRepository->findAll(),
+            'questions' => $questionsRepository->findAll(),
         ]);
     }
 }
