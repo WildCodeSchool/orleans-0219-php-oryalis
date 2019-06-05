34306bc05f3adce738ce599e4d84653be551b4a6
Elodie Billon <elodie.billon1995@outlook.fr>
2019-06-05 14:02:34 +0200

    Suppression du Controller QCM et de la vue suite au merge de dev, ajout du qcm en include au home.html.twig

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,7 +8,10 @@
 
 namespace App\Controller;
 
+use App\Repository\QCMAnswersRepository;
+use App\Repository\QCMQuestionsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
@@ -16,8 +19,11 @@ class HomeController extends AbstractController
     /**
      * @Route("/",name="index")
      */
-    public function index()
+    public function index(QCMAnswersRepository $answersRepository, QCMQuestionsRepository $questionsRepository): Response
     {
-        return $this->render('home.html.twig');
+        return $this->render('home.html.twig', [
+            'answers' => $answersRepository->findAll(),
+            'questions' => $questionsRepository->findAll(),
+            ]);
     }
 }
