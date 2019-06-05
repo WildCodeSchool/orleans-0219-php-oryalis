0428017880b8a71ae852e4f3c233aa2ffa1d1865
Elodie Billon <elodie.billon1995@outlook.fr>
2019-06-05 10:27:12 +0200

    Correction du nom du HomeController

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,7 +11,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeeController extends AbstractController
+class HomeController extends AbstractController
 {
     /**
      * @Route("/",name="index")
