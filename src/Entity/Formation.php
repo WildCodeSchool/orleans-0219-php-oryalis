8ed5d44306f7b321b4ad9cda938f40c1ae456c38
Jessica Salvat <jsalvat7@gmail.com>
2019-06-11 14:08:15 +0200

    refactoring entity

---
+++ src/Entity/Formation.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\FormationRepository")
+ */
+class Formation
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $titre;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $duree;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $public;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $pedagogie;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $formateur;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $programme;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getDuree(): ?string
+    {
+        return $this->duree;
+    }
+
+    public function setDuree(string $duree): self
+    {
+        $this->duree = $duree;
+
+        return $this;
+    }
+
+    public function getPublic(): ?string
+    {
+        return $this->public;
+    }
+
+    public function setPublic(string $public): self
+    {
+        $this->public = $public;
+
+        return $this;
+    }
+
+    public function getPedagogie(): ?string
+    {
+        return $this->pedagogie;
+    }
+
+    public function setPedagogie(string $pedagogie): self
+    {
+        $this->pedagogie = $pedagogie;
+
+        return $this;
+    }
+
+    public function getFormateur(): ?string
+    {
+        return $this->formateur;
+    }
+
+    public function setFormateur(string $formateur): self
+    {
+        $this->formateur = $formateur;
+
+        return $this;
+    }
+
+    public function getProgramme(): ?string
+    {
+        return $this->programme;
+    }
+
+    public function setProgramme(string $programme): self
+    {
+        $this->programme = $programme;
+
+        return $this;
+    }
+}
