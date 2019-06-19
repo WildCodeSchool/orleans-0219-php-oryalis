7d455ce93b229f2fd552cdddf97acef944c37ef7
Jessica Salvat <jsalvat7@gmail.com>
2019-06-19 09:11:38 +0200

    ADD formation

--- src/Entity/Training.php
+++ src/Entity/Training.php
@@ -4,6 +4,8 @@ namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 
+use Symfony\Component\Validator\Constraints as Assert;
+
 /**
  * @ORM\Entity(repositoryClass="App\Repository\TrainingRepository")
  */
@@ -17,34 +19,59 @@ class Training
     private $id;
 
     /**
+     * @Assert\Length(max=255)
+     * @Assert\NotNull()
      * @ORM\Column(type="string", length=255)
      */
     private $title;
 
     /**
+     * @Assert\Length(max=255)
+     * @Assert\NotNull()
      * @ORM\Column(type="string", length=255)
      */
     private $period;
 
     /**
+     * @Assert\Length(max=255)
+     * @Assert\NotNull()
      * @ORM\Column(type="string", length=255)
      */
     private $public;
 
     /**
+     * @Assert\Length(max=255)
+     * @Assert\NotNull()
      * @ORM\Column(type="string", length=255)
      */
     private $pedagogy;
 
     /**
+     * @Assert\Length(max=255)
+     * @Assert\NotNull()
      * @ORM\Column(type="string", length=255)
      */
     private $trainer;
 
     /**
+     * @Assert\NotNull()
      * @ORM\Column(type="text")
      */
     private $program;
+
+    /**
+     * @Assert\NotNull()
+     * @ORM\Column(type="text")
+     */
+    private $goal;
+
+    /**
+     * @Assert\Length(max=255)
+     * @Assert\NotNull()
+     * @ORM\Column(type="string", length=255)
+     */
+    private $prerequisite;
+
 
     public function getId(): ?int
     {
@@ -119,6 +146,30 @@ class Training
     public function setProgram(string $program): self
     {
         $this->program = $program;
+
+        return $this;
+    }
+
+    public function getGoal(): ?string
+    {
+        return $this->goal;
+    }
+
+    public function setGoal(string $goal): self
+    {
+        $this->goal = $goal;
+
+        return $this;
+    }
+
+    public function getPrerequisite(): ?string
+    {
+        return $this->prerequisite;
+    }
+
+    public function setPrerequisite(string $prerequisite): self
+    {
+        $this->prerequisite = $prerequisite;
 
         return $this;
     }
