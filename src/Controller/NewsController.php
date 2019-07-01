4e26d43269a0c4cfd01b8e4ac4345c39e504684f
Elodie Billon <elodie.billon1995@outlook.fr>
2019-07-01 11:55:12 +0200

    Controller corrigé

--- src/Controller/NewsController.php
+++ src/Controller/NewsController.php
@@ -2,12 +2,15 @@
 
 namespace App\Controller;
 
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use App\Entity\News;
 use App\Form\NewsType;
 use App\Repository\NewsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -30,26 +33,29 @@ class NewsController extends AbstractController
     }
 
     /**
-     * @Route("/new", name="news_new", methods={"GET","POST"})
+     * @Route("/ajouter-une-actualite", name="news_new", methods={"GET","POST"})
      */
-    public function new(Request $request): Response
+    public function new(Request $request, ObjectManager $manager): Response
     {
         $news = new News();
         $form = $this->createForm(NewsType::class, $news);
+        $form = $this->createFormBuilder($news)
+            ->add('name', TextType::class)
+            ->add('date', DateType::class, [
+                'widget' => 'choice',
+                'format' => 'dd-MM-yyyy',
+                'years' => range(2019,2069,1)
+            ])
+            ->add('content', TextareaType::class)
+            ->getForm();
         $form->handleRequest($request);
-        $form->add('date', DateType::class, [
-            'widget' => 'choice',
-            'format' => 'dd-MM-yyyy',
-            'years' => range(date('Y'), date('Y')+50),
-        ]);
-        $form->add('content', TextareaType::class);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($news);
-            $entityManager->flush();
+            $manager ->persist($news);
+            $manager->flush();
 
             return $this->redirectToRoute('news_index');
+//            return $this->redirectToRoute('news_show', ['id' => $news->getId()]);
         }
 
         return $this->render('admin_news/new.html.twig', [
