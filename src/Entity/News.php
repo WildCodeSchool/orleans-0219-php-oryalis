d33975d85bdb679fe250737bbdf2ddedf8320aab
Elodie Billon <elodie.billon1995@outlook.fr>
2019-07-01 14:10:05 +0200

    validator commencé

--- src/Entity/News.php
+++ src/Entity/News.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints\NotBlank as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\NewsRepository")
@@ -18,6 +19,7 @@ class News
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank
      */
     private $name;
 
