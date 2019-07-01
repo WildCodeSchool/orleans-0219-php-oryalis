e691304562b496e4cfb6114ef4b3177169ab8e81
Elodie Billon <elodie.billon1995@outlook.fr>
2019-07-01 18:04:00 +0200

    liste des qcm fonctionnelle

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -8,13 +8,26 @@
 
 namespace App\Controller;
 
+use App\Repository\AnswerRepository;
+use App\Repository\QuestionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class QuestionController extends AbstractController
 {
-    public function index() : Request
+    /**
+     * @Route("/history", name="questions_history")
+     * @param QuestionRepository $questionRepository
+     * @return Response
+     */
+    public function index(QuestionRepository $questionRepository, AnswerRepository $answerRepository) : Response
     {
-        $this->render('questions/questions.html.twig');
+        return $this->render('questions/questions.html.twig', [
+            $question = $questionRepository->findAll(),
+            $answers = $answerRepository->findByQuestion($question),
+            'questions' => $question,
+            'answers' => $answers
+        ]);
     }
 }
