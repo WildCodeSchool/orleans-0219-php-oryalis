cc5adf1c83a884e7b7d67b9d4225103ed4a96887
Elodie Billon <elodie.billon1995@outlook.fr>
2019-07-03 09:47:25 +0200

    StandardsController renommé au singulier

--- src/Controller/StandardController.php
+++ src/Controller/StandardController.php
@@ -13,7 +13,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class StandardsController extends AbstractController
+class StandardController extends AbstractController
 {
     /**
      * @Route("/normes", name="standards_index")
