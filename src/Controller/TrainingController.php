27d9d01d9a695387fdf6e5b26b4939d32bbe4ce6
dimxpas <dimitri_du_28@hotmail.fr>
2019-07-04 18:25:32 +0200

    Ajout du pdf, stylisation du pdf, modification de la page formations

--- src/Controller/TrainingController.php
+++ src/Controller/TrainingController.php
@@ -11,6 +11,9 @@ namespace App\Controller;
 
 use App\Entity\Training;
 use App\Repository\TrainingRepository;
+use Knp\Bundle\SnappyBundle\KnpSnappyBundle;
+use Knp\Bundle\SnappyBundle\Snappy\Response\PdfResponse;
+use Knp\Snappy\Pdf;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -20,7 +23,6 @@ use Symfony\Component\Routing\Annotation\Route;
  * @package App\Controller
  * @Route("/formations")
  */
-
 class TrainingController extends AbstractController
 {
     /**
@@ -31,8 +33,22 @@ class TrainingController extends AbstractController
     public function index(TrainingRepository $trainingRepository): Response
     {
         return $this->render('formations/trainingList.html.twig', [
-                'trainings' => $trainingRepository->findAll()
-            ]);
+            'trainings' => $trainingRepository->findAll(),
+        ]);
+    }
+    /**
+     * @Route("/{id}/download", name="training_download", methods={"GET"})
+     * @param Training $training
+     * @param Pdf $knpSnappyBundle
+     * @return Response
+     */
+    public function donwload(Training $training, Pdf $knpSnappyBundle): Response
+    {
+        $html = $this->renderView('formations/trainingPDF.html.twig', ['training' => $training]);
+        return new PdfResponse(
+            $knpSnappyBundle->getOutputFromHtml($html, ['user-style-sheet' => ['./build/pdf.css',]]),
+            'training' . $training->getId() . '.pdf'
+        );
     }
 
     /**
@@ -43,7 +59,7 @@ class TrainingController extends AbstractController
     public function show(Training $training): Response
     {
         return $this->render('formations/show.html.twig', [
-            'training' => $training
+            'training' => $training,
         ]);
     }
 }
