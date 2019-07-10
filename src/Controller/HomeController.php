6ea6af926b24427cf687b34ac44099452f05f0f7
dimxpas <dimitri_du_28@hotmail.fr>
2019-07-10 17:30:56 +0200

    Affichage de la dernière news qui était en dure sur la homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\AnswerRepository;
+use App\Repository\NewsRepository;
 use App\Repository\QuestionRepository;
 use Feed;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -18,16 +19,21 @@ class HomeController extends AbstractController
      * @return Response
      */
 
-    public function index(AnswerRepository $answersRepository, QuestionRepository $questionsRepository):Response
-    {
+    public function index(
+        AnswerRepository $answersRepository,
+        QuestionRepository $questionsRepository,
+        NewsRepository $newsRepository
+    ): Response {
         $rss = Feed::loadRss('http://qualite-securite-environnement.oryalis.com/feed/');
         $rss = $rss->item;
         return $this->render('home/index.html.twig', [
             $question = $questionsRepository->findOneBy([], ['year' => 'DESC', 'month' => 'DESC']),
             $answers = $answersRepository->findByQuestion($question),
+            $news = $newsRepository->findOneBy([], ['date' => 'DESC']),
             'feeds' => $rss,
             'question' => $question,
             'answers' => $answers,
+            'news' => $news,
         ]);
     }
 }
